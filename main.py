64a8381c6f689f4e0a599bee8f3a9e13674f829f
Buranima <91853200+Buranima@users.noreply.github.com>
2022-09-12 00:59:17 +0700

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 def ยิง():
-    global ยาน
+    global ความเร็ว
     if กระสุน.is_touching(ยาน):
+        game.add_score(1)
         กระสุน.delete()
-        if ยาน == ยาน:
-            if ยาน.get(LedSpriteProperty.X) == 0:
-                ยาน.delete()
-                ยาน = game.create_sprite(randint(1, 4), 0)
-            elif ยาน.get(LedSpriteProperty.X) == 1:
-                while ยาน.get(LedSpriteProperty.X) == 1:
-                    ยาน.delete()
-                    ยาน = game.create_sprite(randint(0, 4), 0)
-            elif ยาน.get(LedSpriteProperty.X) == 2:
-                while ยาน.get(LedSpriteProperty.X) == 2:
-                    ยาน.delete()
-                    ยาน = game.create_sprite(randint(0, 4), 0)
-            elif ยาน.get(LedSpriteProperty.X) == 3:
-                while ยาน.get(LedSpriteProperty.X) == 3:
-                    ยาน.delete()
-                    ยาน = game.create_sprite(randint(0, 4), 0)
-            elif ยาน.get(LedSpriteProperty.X) == 4:
-                while ยาน.get(LedSpriteProperty.X) == 4:
-                    ยาน.delete()
-                    ยาน = game.create_sprite(randint(0, 4), 0)
+        ความเร็ว = ความเร็ว + -10
+        สร้างยาน()
     elif กระสุน.get(LedSpriteProperty.Y) == 0:
         กระสุน.delete()
 
@@ -31,11 +14,47 @@ def on_button_pressed_a():
     กระสุน.turn(Direction.LEFT, 90)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def นับคะแนน():
+    game.game_over()
+    basic.show_number(game.score())
+def สร้างยาน():
+    global ยาน
+    if ยาน == ยาน:
+        if ยาน.get(LedSpriteProperty.X) == 0:
+            ยาน.delete()
+            ยาน = game.create_sprite(randint(1, 4), 0)
+        elif ยาน.get(LedSpriteProperty.X) == 1:
+            while ยาน.get(LedSpriteProperty.X) == 1:
+                ยาน.delete()
+                ยาน = game.create_sprite(randint(0, 4), 0)
+        elif ยาน.get(LedSpriteProperty.X) == 2:
+            while ยาน.get(LedSpriteProperty.X) == 2:
+                ยาน.delete()
+                ยาน = game.create_sprite(randint(0, 4), 0)
+        elif ยาน.get(LedSpriteProperty.X) == 3:
+            while ยาน.get(LedSpriteProperty.X) == 3:
+                ยาน.delete()
+                ยาน = game.create_sprite(randint(0, 4), 0)
+        elif ยาน.get(LedSpriteProperty.X) == 4:
+            while ยาน.get(LedSpriteProperty.X) == 4:
+                ยาน.delete()
+                ยาน = game.create_sprite(randint(0, 4), 0)
+
+def on_button_pressed_b():
+    global กระสุน
+    กระสุน = game.create_sprite(ปืน.get(LedSpriteProperty.X), ปืน.get(LedSpriteProperty.Y))
+    กระสุน.turn(Direction.LEFT, 90)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 กระสุน: game.LedSprite = None
 ยาน: game.LedSprite = None
 ปืน: game.LedSprite = None
+ความเร็ว = 0
+ความเร็ว = 200
 ปืน = game.create_sprite(2, 4)
 ยาน = game.create_sprite(randint(0, 4), 0)
+ตาย = 0
+game.start_countdown(10000)
 
 def on_forever():
     ปืน.move(1)
@@ -43,5 +62,5 @@ def on_forever():
     if กระสุน:
         กระสุน.move(1)
         ยิง()
-    basic.pause(500)
+    basic.pause(ความเร็ว)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,49 +1,67 @@
 function ยิง () {
     if (กระสุน.isTouching(ยาน)) {
+        game.addScore(1)
         กระสุน.delete()
-        if (ยาน == ยาน) {
-            if (ยาน.get(LedSpriteProperty.X) == 0) {
-                ยาน.delete()
-                ยาน = game.createSprite(randint(1, 4), 0)
-            } else if (ยาน.get(LedSpriteProperty.X) == 1) {
-                while (ยาน.get(LedSpriteProperty.X) == 1) {
-                    ยาน.delete()
-                    ยาน = game.createSprite(randint(0, 4), 0)
-                }
-            } else if (ยาน.get(LedSpriteProperty.X) == 2) {
-                while (ยาน.get(LedSpriteProperty.X) == 2) {
-                    ยาน.delete()
-                    ยาน = game.createSprite(randint(0, 4), 0)
-                }
-            } else if (ยาน.get(LedSpriteProperty.X) == 3) {
-                while (ยาน.get(LedSpriteProperty.X) == 3) {
-                    ยาน.delete()
-                    ยาน = game.createSprite(randint(0, 4), 0)
-                }
-            } else if (ยาน.get(LedSpriteProperty.X) == 4) {
-                while (ยาน.get(LedSpriteProperty.X) == 4) {
-                    ยาน.delete()
-                    ยาน = game.createSprite(randint(0, 4), 0)
-                }
-            }
-        }
+        ความเร็ว = ความเร็ว + -5
+        สร้างยาน()
+        เช็คปืน = true
     } else if (กระสุน.get(LedSpriteProperty.Y) == 0) {
         กระสุน.delete()
+        เช็คปืน = true
     }
 }
 input.onButtonPressed(Button.A, function () {
-    กระสุน = game.createSprite(ปืน.get(LedSpriteProperty.X), ปืน.get(LedSpriteProperty.Y))
-    กระสุน.turn(Direction.Left, 90)
+    if (เช็คปืน) {
+        เช็คปืน = false
+        กระสุน = game.createSprite(ปืน.get(LedSpriteProperty.X), ปืน.get(LedSpriteProperty.Y))
+        กระสุน.turn(Direction.Left, 90)
+    }
 })
+function สร้างยาน () {
+    if (ยาน == ยาน) {
+        if (ยาน.get(LedSpriteProperty.X) == 0) {
+            ยาน.delete()
+            ยาน = game.createSprite(randint(1, 4), 0)
+        } else if (ยาน.get(LedSpriteProperty.X) == 1) {
+            while (ยาน.get(LedSpriteProperty.X) == 1) {
+                ยาน.delete()
+                ยาน = game.createSprite(randint(0, 4), 0)
+            }
+        } else if (ยาน.get(LedSpriteProperty.X) == 2) {
+            while (ยาน.get(LedSpriteProperty.X) == 2) {
+                ยาน.delete()
+                ยาน = game.createSprite(randint(0, 4), 0)
+            }
+        } else if (ยาน.get(LedSpriteProperty.X) == 3) {
+            while (ยาน.get(LedSpriteProperty.X) == 3) {
+                ยาน.delete()
+                ยาน = game.createSprite(randint(0, 4), 0)
+            }
+        } else if (ยาน.get(LedSpriteProperty.X) == 4) {
+            while (ยาน.get(LedSpriteProperty.X) == 4) {
+                ยาน.delete()
+                ยาน = game.createSprite(randint(0, 4), 0)
+            }
+        }
+    }
+}
 input.onButtonPressed(Button.B, function () {
-    กระสุน = game.createSprite(ปืน.get(LedSpriteProperty.X), ปืน.get(LedSpriteProperty.Y))
-    กระสุน.turn(Direction.Left, 90)
+    if (เช็คปืน) {
+        เช็คปืน = false
+        กระสุน = game.createSprite(ปืน.get(LedSpriteProperty.X), ปืน.get(LedSpriteProperty.Y))
+        กระสุน.turn(Direction.Left, 90)
+    }
 })
 let กระสุน: game.LedSprite = null
+let เช็คปืน = false
 let ยาน: game.LedSprite = null
 let ปืน: game.LedSprite = null
+let ความเร็ว = 0
+ความเร็ว = 200
 ปืน = game.createSprite(2, 4)
 ยาน = game.createSprite(randint(0, 4), 0)
+เช็คปืน = true
+game.startCountdown(10000)
 basic.forever(function () {
     ปืน.move(1)
     ปืน.ifOnEdgeBounce()
@@ -51,5 +69,5 @@ basic.forever(function () {
         กระสุน.move(1)
         ยิง()
     }
-    basic.pause(200)
+    basic.pause(ความเร็ว)
 })
