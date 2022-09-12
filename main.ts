273973035fd3e77c4442ea08a022fffad8c0db5d
Buranima <91853200+Buranima@users.noreply.github.com>
2022-09-12 20:06:04 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 function ยิง () {
     if (กระสุน.isTouching(ยาน)) {
-        game.addScore(1)
+        game.addScore(10)
         กระสุน.delete()
-        ความเร็ว = ความเร็ว + -10
+        ความเร็ว = ความเร็ว + -1
         สร้างยาน()
         เช็คปืน = true
+        ชีวิต = ชีวิต + 1
     } else if (กระสุน.get(LedSpriteProperty.Y) == 0) {
         กระสุน.delete()
         เช็คปืน = true
@@ -15,8 +16,21 @@ input.onButtonPressed(Button.A, function () {
         กระสุน = game.createSprite(ปืน.get(LedSpriteProperty.X), ปืน.get(LedSpriteProperty.Y))
         กระสุน.turn(Direction.Left, 90)
         เช็คปืน = false
+        ชีวิต = ชีวิต - 1
+    }
+    if (ชีวิต <= 2) {
+        ตาย()
     }
 })
+function ตาย () {
+    if (เตือน) {
+        basic.showIcon(IconNames.Heart)
+        เตือน = false
+    }
+    if (ชีวิต == 0) {
+        game.gameOver()
+    }
+}
 function สร้างยาน () {
     if (ยาน == ยาน) {
         if (ยาน.get(LedSpriteProperty.X) == 0) {
@@ -50,9 +64,15 @@ input.onButtonPressed(Button.B, function () {
         กระสุน = game.createSprite(ปืน.get(LedSpriteProperty.X), ปืน.get(LedSpriteProperty.Y))
         กระสุน.turn(Direction.Left, 90)
         เช็คปืน = false
+        ชีวิต = ชีวิต - 1
+    }
+    if (ชีวิต <= 2) {
+        ตาย()
     }
 })
 let กระสุน: game.LedSprite = null
+let เตือน = false
+let ชีวิต = 0
 let เช็คปืน = false
 let ยาน: game.LedSprite = null
 let ปืน: game.LedSprite = null
@@ -60,8 +80,9 @@ let ความเร็ว = 0
 ความเร็ว = 200
 ปืน = game.createSprite(2, 4)
 ยาน = game.createSprite(randint(0, 4), 0)
-game.startCountdown(40000)
 เช็คปืน = true
+ชีวิต = 5
+เตือน = true
 basic.forever(function () {
     ปืน.move(1)
     ปืน.ifOnEdgeBounce()
